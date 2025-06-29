23f52251713785247ccdda45375bde9dfe5d6584
Gemkodor <gemkodor@gmail.com>
2025-06-29 07:29:58 +0200

    Update allowed hosts

--- muziart/settings.py
+++ muziart/settings.py
@@ -30,7 +30,10 @@ SECRET_KEY = os.environ.get('SECRET_KEY', default='django-insecure-bbaks=nyh*dzk
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'RENDER' not in os.environ
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['api.muziart.fr']
+
+if DEBUG:
+    ALLOWED_HOSTS.append('localhost')
 
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:

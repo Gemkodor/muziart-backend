5f687e9f4f98bc602da2c481f2398760ef06b836
Gemkodor <gemkodor@gmail.com>
2026-01-12 22:38:32 +0100

    Return only 10 instruments

--- core/views_games.py
+++ core/views_games.py
@@ -3,5 +3,5 @@ from .models import  Instrument
 
 
 def instruments_list(request):
-    instruments = Instrument.objects.all().order_by('?').values()
+    instruments = Instrument.objects.all().order_by('?')[:10].values()
     return JsonResponse(list(instruments), safe=False)
